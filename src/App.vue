b848c770ea7d1f982a86e3cf7294a00d93bd9712
Nithin Mahesh <nmahesh.informatics@gmail.com>
2024-03-09 20:10:26 -0500

    Adding setup files - still tinkering around with them

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      YouTube
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+  <header>
+      <div class="top-header">
+          <div class="text">Trinbault Corporation</div>
+          <!-- Logo -->
+          <!-- <div id="logo">
+              <img src="../src/assets/logo.png" />
+          </div> -->
+          
+      </div>
+      <div id="header-image-menu">
+      </div>
+  </header>
+
+  
+
+  <RouterView />
+</template>
+
+<style scoped>
+/* .text {
+  position: relative;
+  float: left;
+  top: -280px;
+  left: 10px;
+  width: 150px;
+  height: 300px;
+  padding: 5px;
+  border: solid 1px red;
+  background: #ffffff;
+} */
+.top-header {
+  height: 7vh;
+  overflow: auto;
+  margin-top: 1em;
+  border: solid 1px red;
+}
+.text {
+  position: sticky;
+  /* top: 0px; */
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import NavigationHeaders from '../components/NavigationHeaders.vue'
+</script>
+
+<template>
+  <main class="pageMargins">
+    <!-- TODO header -->
+    <!-- <NavigationHeaders /> -->
+    <!-- TODO create grid components -->
+
+    <!-- TODO create left side nav bar like youtube -->
+  </main>
+</template>
+
+<style scoped>
+.pageMargins {
+  margin-top: -15px;
+}
+</style>
